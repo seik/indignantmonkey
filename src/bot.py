59d1844ddb2997cb165daa55c06a9e7412b797c2
Iván Martín <ivanmartinga@gmail.com>
2023-10-12 14:03:40 +0200

    Simplify deployment

--- src/bot.py
+++ src/bot.py
@@ -12,7 +12,7 @@ from telegram import (
     InlineQueryResultCachedGif,
     ParseMode,
 )
-from telegram.ext import Dispatcher, CommandHandler, InlineQueryHandler
+from telegram.ext import Updater, Dispatcher, CommandHandler, InlineQueryHandler
 
 from src.image_utils import text_wrap
 
@@ -22,13 +22,6 @@ if logger.handlers:
         logger.removeHandler(handler)
 logging.basicConfig(level=logging.INFO)
 
-OK_RESPONSE = {
-    "statusCode": 200,
-    "headers": {"Content-Type": "application/json"},
-    "body": json.dumps("ok"),
-}
-ERROR_RESPONSE = {"statusCode": 400, "body": json.dumps("Oops, something went wrong!")}
-
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 
 STORAGE_CHAT_ID = os.environ.get("STORAGE_CHAT_ID")
@@ -36,43 +29,6 @@ STORAGE_CHAT_ID = os.environ.get("STORAGE_CHAT_ID")
 MONKEY_IMAGE_PATH = "src/statics/monkey.gif"
 
 FONT_PATH = "src/statics/font.ttf"
-
-
-def configure_telegram():
-    """
-    Configures the bot with a Telegram Token.
-    Returns a bot instance.
-    """
-
-    telegram_token = os.environ.get("TELEGRAM_TOKEN")
-    if not telegram_token:
-        logger.error("The TELEGRAM_TOKEN must be set")
-        raise NotImplementedError
-
-    return Bot(telegram_token)
-
-
-def set_up_dispatcher(dispatcher: Dispatcher) -> None:
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("monkey", monkey))
-    inline_caps_handler = InlineQueryHandler(inline_monkey)
-    dispatcher.add_handler(inline_caps_handler)
-
-
-bot = configure_telegram()
-dispatcher = Dispatcher(bot, None, use_context=True)
-
-
-def handler(event, context) -> dict:
-    logger.info(f"Event: {event}")
-
-    try:
-        dispatcher.process_update(Update.de_json(json.loads(event.get("body")), bot))
-    except Exception as e:
-        logger.error(e)
-        return ERROR_RESPONSE
-
-    return OK_RESPONSE
 
 
 def start(update: Update, context: dict) -> None:
@@ -86,6 +42,10 @@ def start(update: Update, context: dict) -> None:
         chat_id=update.effective_chat.id, text=text, parse_mode=ParseMode.MARKDOWN
     )
     send_monkey(update.effective_chat.id, text="AAAAAA")
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
 
 
 def monkey(update: Update, context: dict) -> None:
@@ -162,4 +122,32 @@ def get_monkey_text_bytes(text) -> Union[io.BytesIO, BinaryIO]:
     return monkey_bytes
 
 
-set_up_dispatcher(dispatcher)
+def main():
+    token = str(os.environ['TELEGRAM_TOKEN'])
+
+    bot = Bot(token)
+
+    updater = Updater(bot)
+
+    dispatcher = Dispatcher(bot, None, use_context=True)
+
+    # commands
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("monkey", monkey))
+    inline_caps_handler = InlineQueryHandler(inline_monkey)
+    dispatcher.add_handler(inline_caps_handler)
+    dp.add_handler(CommandHandler("shot", shot))
+
+    updater.start_polling()
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Block until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
